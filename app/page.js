530890607209d92b43deada8a459d2680c0031ae
rtder6023 <rtder6023@gmail.com>
2026-09-07 16:39:58 +0900

    feat: 스피키?로 추정되는 것을 추가

--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
+"use client";
+import { playSpeakiSound } from "./utils/sound";
+
 export default function Page() {
   return (
     <html>
       <title>speaki</title>
       <body>
         <h1>스피키 네르기</h1>
+        <button onClick={playSpeakiSound}>스피키임 (아무튼 스피키)</button>
       </body>
     </html>
   );
