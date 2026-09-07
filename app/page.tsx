2578d7f1f8cf2ddf7eef0a38f1ce47d93e34166c
rtder6023 <rtder6023@gmail.com>
2026-09-07 17:51:24 +0900

    chore: JS 버려잇

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,13 @@ import { Toaster } from "sonner";
 
 export default function Page() {
   return (
-    <html>
+    <>
       <title>speaki</title>
       <body>
         <h1>스피키 네르기</h1>
         <button onClick={playSpeakiSound}>스피키임 (아무튼 스피키)</button>
         <Toaster position="bottom-center" />
       </body>
-    </html>
+    </>
   );
 }
